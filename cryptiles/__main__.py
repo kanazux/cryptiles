5a898922ff72e20baa591e4c6cebaca155ff5377
Silvio Silva | Stone <silvio.silva@stone.local@BRRIOLN012383.stone.local>
2019-04-06 12:52:44 -0300

    Added decode method to bytes object

--- cryptiles/__main__.py
+++ cryptiles/__main__.py
@@ -3,6 +3,7 @@
 
 
 import os
+import binascii
 from .cryptile_parse import parse_args
 from .cryptile import crypt_data
 
@@ -15,15 +16,18 @@ def main():
         _file = open(args.file, 'r').read()
         with open(args.file, 'w') as crypt_file:
             if args.encrypt:
-                crypt_file.write(_crypt.encrypt(_file))
+                crypt_file.write(_crypt.encrypt(_file).decode())
             if args.decrypt:
-                crypt_file.write(_crypt.decrypt(_file))
+                crypt_file.write(_crypt.decrypt(_file).decode())
 
     if args.string:
         if args.encrypt:
-            print(_crypt.encrypt(args.string))
-        if args.decrypt:
-            print(_crypt.decrypt(args.string))
+            print(_crypt.encrypt(args.string).decode())
+        try:
+            if args.decrypt:
+                print(_crypt.decrypt(args.string).decode())
+        except binascii.Error:
+                print("This string is not a base64 string.")
 
 if __name__ == "__main__":
     main()
